8329c1ee356e3bd40a8e7a10381f106bd9299526
butrico31 <butrico0@gmail.com>
2024-11-06 14:13:57 -0300

    teste

--- src/main/java/com/site/chatteste/config/ConnectorConfig.java
+++ src/main/java/com/site/chatteste/config/ConnectorConfig.java
@@ -24,16 +24,7 @@ public class ConnectorConfig {
                 context.addConstraint(securityConstraint);
             }
         };
-        tomcat.addAdditionalTomcatConnectors(getHttpConnector());
+        
         return tomcat;
-    }
-
-    private Connector getHttpConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("https");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(8443);
-        return connector;
     }
 }
