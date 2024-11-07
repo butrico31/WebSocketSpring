9da6b0d1904b88c68fcf936d1b3a1649b257becb
butrico31 <butrico0@gmail.com>
2024-11-07 20:58:33 -0300

    oi

--- src/main/java/com/site/chatteste/App.java
+++ src/main/java/com/site/chatteste/App.java
@@ -1,5 +1,6 @@
 package com.site.chatteste;
 
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
